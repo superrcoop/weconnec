5f7704466727f537ddc7fd2f23f69b8f3cd2173b
Jordan Cooper <jordan.cooper3000@gmail.com>
2018-05-19 22:49:49 -0500

    add accordion options to profile

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -281,7 +281,8 @@ Vue.component('app-footer', {
 
 Vue.component('card', {
   template: `
-  <div class="card mb-3">
+
+  <div class="card ">
   <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif"  alt="Card image cap">
   <div class="card-body">
     <h5 class="card-title">{{title}}</h5>
@@ -622,12 +623,24 @@ const Profile = Vue.component('profile',{
               <p>Profile preview</p>
             </div>
           </div>
-          <div class="card my-4">
-            <h5 class="card-header">More options</h5>
-            <div class="card-body">
-              You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
-            </div>
-          </div>
+          <div class="accordion" id="accordionExample">
+  <div class="card my-4">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          More options
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+
+  </div>
         </div>
         <div class="col-md-8">
           <h1>Resources</h1><a href="#upload-modal" data-toggle="modal">Upload new</a>
